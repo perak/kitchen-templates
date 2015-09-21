3e7cafd44a07fbbc394ae353d95ad0486def573a
Kadir ERDOGAN <ekadir@metu.edu.tr>
2015-09-22 01:45:11 +0300

    Initial commit of materialize theme

---
+++ ui/materialize/components/data_view_table_items.js
@@ -0,0 +1,53 @@
+Template.TEMPLATE_NAME.rendered = function() {
+	/*TEMPLATE_RENDERED_CODE*/
+};
+
+Template.TEMPLATE_NAME.events({
+	"click td": function(e, t) {
+		e.preventDefault();
+		/*DETAILS_ROUTE*/
+		return false;
+	},
+
+	"click .inline-checkbox": function(e, t) {
+		e.preventDefault();
+
+		if(!this || !this._id) return false;
+
+		var fieldName = $(e.currentTarget).attr("data-field");
+		if(!fieldName) return false;
+
+		var values = {};
+		values[fieldName] = !this[fieldName];
+
+		COLLECTION_VAR.update({ _id: this._id }, { $set: values });
+
+		return false;
+	},
+
+	"click #delete-button": function(e, t) {
+		e.preventDefault();
+		var me = this;
+		  $('.modal').openModal({
+			  dismissible: true, // Modal can be dismissed by clicking outside of the modal
+			  opacity: .5, // Opacity of modal background
+			  in_duration: 300, // Transition in duration
+			  out_duration: 200, // Transition out duration
+			  ready: function() { }, // Callback for Modal open
+			  complete: function(e) { console.log(e);/*COLLECTION_VAR.remove({ _id: me._id });*/ } // Callback for Modal close
+			});
+		return false;
+	},
+	"click #edit-button": function(e, t) {
+		e.preventDefault();
+		/*EDIT_ROUTE*/
+		return false;
+	}
+});
+
+Template.TEMPLATE_NAME.helpers({
+	"checked": function(value) { return value ? "checked" : "" }
+	/*EDIT_BUTTON_CLASS_HELPER*/
+
+	/*DELETE_BUTTON_CLASS_HELPER*/
+});
